094edcbd4cb7be9148ea40c63d2b941647b35697
alenn85 <alenn.ong@cell-wellbeing.com>
2022-04-28 06:23:37 +0800

    event binding practice

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No server was created.";
 
   constructor() {
     // function () {}
@@ -16,6 +17,11 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer()
+  {
+    this.serverCreationStatus = "Server was created!";
   }
 
 }
