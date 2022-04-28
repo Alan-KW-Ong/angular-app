d9c14a80bce440316a60272a48363a0f33c1110b
Alan-KW-Ong <alan37okw@gmail.com>
2022-04-28 12:43:07 +0800

    test with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,8 +9,18 @@ export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'Offline';
 
+    constructor()
+    {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
     getServerStatus()
     {
         return this.serverStatus;
+    }
+
+    getColor()
+    {
+        return this.serverStatus === 'Online' ? 'green' : 'red';
     }
 }
